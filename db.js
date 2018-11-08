d6236802e9a2dc8bc4fbcd7ceb0b46f86aa45e18
scottjstewart <scottstewart965@gmail.com>
2018-11-08 13:45:15 -0500

    kmdsl'agmfsleknmglkmrdlkgm

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 const Sequelize = require("sequelize");
 
-new Sequelize(
+const sequelize = new Sequelize(
   process.env.DATABASE_URL ||
     `postgresql://postgres:${encodeURIComponent(
       process.env.PASS
